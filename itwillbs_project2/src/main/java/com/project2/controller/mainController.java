6b2528cd8165d7f728697e15c133c8b2554dde70
nirong2 <not.garan02@gmail.com>
2023-10-30 12:26:57 +0900

    main 페이지 연결 Controller 다시 생성_ 메인 연결 수정 필요

---
+++ itwillbs_project2/src/main/java/com/project2/controller/mainController.java
@@ -0,0 +1,25 @@
+package com.project2.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class mainController {
+
+	private static final Logger logger = LoggerFactory.getLogger(mainController.class);
+	
+	
+	//http://localhost:8088/main
+	@RequestMapping(value = "/main", method = RequestMethod.GET)
+	public String mainGET() {
+
+		return "/home";
+	}
+	
+	
+}
