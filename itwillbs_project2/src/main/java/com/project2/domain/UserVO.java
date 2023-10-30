4f76bd995f6bf16ef1869be4ddf9ec4242bf0d8a
00pow <kimseo131612@gmail.com>
2023-10-31 01:20:02 +0900

    User 회원가입 관련  - userVO, UserMapper

---
+++ itwillbs_project2/src/main/java/com/project2/domain/UserVO.java
@@ -0,0 +1,26 @@
+package com.project2.domain;
+
+import java.sql.Timestamp;
+
+import lombok.Data;
+
+@Data
+public class UserVO {
+	
+	private Integer user_num;
+	private String user_id;
+	private String user_pw;
+	private String user_name;
+	private String user_phone;
+	private Timestamp user_regdate;
+	private Timestamp user_last_access;
+	private Integer user_type;
+	private Integer user_report_state;
+	private Integer user_delete_state;
+	
+	
+	
+	
+	
+
+}
